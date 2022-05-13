52076b652ef227126944949e074a60a4cd9b5f7f
Tongze Wang <wtongze@gmail.com>
2022-05-14 01:08:16 +0800

    Init commit

---
+++ .eslintrc.js
@@ -0,0 +1,16 @@
+module.exports = {
+  env: {
+    es6: true,
+    node: true,
+  },
+  extends: ["airbnb-base", "plugin:@typescript-eslint/recommended", "prettier"],
+  parser: "@typescript-eslint/parser",
+  parserOptions: {
+    ecmaVersion: "latest",
+    sourceType: "module",
+  },
+  plugins: ["@typescript-eslint"],
+  rules: {
+    "no-console": "off",
+  },
+};
