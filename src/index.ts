b0ab5ed3dc79312a0b70ff9f86a412d55a5f3a84
Tongze Wang <wtongze@gmail.com>
2022-05-14 01:19:36 +0800

    Add prettier config

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,11 @@
-import express from "express";
-import type { Request, Response } from "express";
+import express from 'express';
+import type { Request, Response } from 'express';
 
 const app = express();
 const port = 3000;
 
-app.get("/", (req: Request, res: Response) => {
-  res.send("Hello World!");
+app.get('/', (req: Request, res: Response) => {
+  res.send('Hello World!');
 });
 
 app.listen(port, () => {
